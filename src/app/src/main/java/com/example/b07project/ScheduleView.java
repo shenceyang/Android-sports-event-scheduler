a88259ec8bdf9b1836046ada3c241bd0799180b6
Mustafa Hasan <mustafahasan1702@gmail.com>
2022-08-07 12:57:44 -0400

    changed layout

--- src/app/src/main/java/com/example/b07project/ScheduleView.java
+++ src/app/src/main/java/com/example/b07project/ScheduleView.java
@@ -1,22 +1,93 @@
 package com.example.b07project;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+
 public class ScheduleView extends AppCompatActivity {
+    private EventPresenter eventPresenter;
+    private VenuePresenter venuePresenter;
     private SchedulePresenter schedulePresenter;
     private DatabaseReference database = FirebaseDatabase.getInstance("https://android-sport-app-default-rtdb.firebaseio.com/").getReference();
-    public ScheduleView() {
+    private String userID;
+
+    private void addUpcomingEvents() {
+        this.eventPresenter.getSortedListEvents(new EventCallback.GetSortedListEventsCallback() {
+            @Override
+            public void getSortedListEventsCallback(List<Event> sortedEvents) {
+                LinearLayout eventList = (LinearLayout) findViewById(R.id.eventList);
+                for(Event event: sortedEvents) {
+//                    Log.d("sortedevents", String.valueOf(event.getEventID()));
+                    LayoutInflater inflater = getLayoutInflater();
+                    View newEvent = inflater.inflate(R.layout.upcoming_events_template, eventList, false);
+
+                    TextView venueText = (TextView) newEvent.findViewById(R.id.eventVenue);
+                    TextView dateText = (TextView) newEvent.findViewById(R.id.eventDate);
+                    TextView sportText = (TextView) newEvent.findViewById(R.id.eventSport);
+                    TextView startText = (TextView) newEvent.findViewById(R.id.eventStartTime);
+                    TextView endText = (TextView) newEvent.findViewById(R.id.eventEndTime);
+                    TextView joinedPlayersText = (TextView) newEvent.findViewById(R.id.eventJoinedPlayers);
+                    TextView maxPlayersText = (TextView) newEvent.findViewById(R.id.eventMaxPlayers);
+                    Button joinButton = (Button) newEvent.findViewById(R.id.joinButton);
+
+                    venuePresenter.getVenue(event.getVenueID(), new VenueCallback.GetVenueCallback() {
+                        @Override
+                        public void getVenueCallback(Venue venue) {
+                            venueText.setText("Venue: " + venue.getVenueName());
+                        }
+                    });
+                    dateText.setText("Date: " + event.getDay() + "/" + event.getMonth() + "/" + event.getYear());
+                    sportText.setText("Sport: " + event.getSport());
+                    startText.setText("Start: " + event.getStartHour() + ":" + String.format("%02d", event.getStartMin()));
+                    endText.setText("End: " + event.getEndHour() + ":" + String.format("%02d", event.getEndMin()));
+                    joinedPlayersText.setText("Players joined: " + event.getCurrPlayers());
+                    maxPlayersText.setText("Max Players: " + event.getMaxPlayers());
+
+                    // Check if current user already joined event
+                    schedulePresenter.isScheduled(userID, event.getEventID(), new ScheduleCallback.IsScheduledCallback() {
+                        @Override
+                        public void isScheduledCallback() {
+                            joinButton.setEnabled(false);
+                        }
+                    });
+
+                    if(event.getMaxPlayers() == event.getCurrPlayers()) {
+                        joinButton.setEnabled(false);
+                    }
+                    else {
+                        joinButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ID id = new ID(database);
+                                id.getNextScheduleID(new IDCallback.GetNextScheduleIDCallback() {
+                                    @Override
+                                    public void getNextScheduleIDCallback(int nextID) {
+                                        Schedule s = new Schedule(nextID, event.getEventID(), userID, event.getVenueID());
+                                        schedulePresenter.pushSchedule(s);
+                                        eventList.removeAllViews();
+                                        addUpcomingEvents();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                    eventList.addView(newEvent);
+                }
+            }
+        });
     }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venues);
-    }
-}
+    }}
